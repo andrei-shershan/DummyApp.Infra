8a910e4d570a6cd4c11ba5694f388bee87e38d52
Andrei Shershan <mail.shershan@gmail.com>
2026-08-24 19:06:58 +0200

    fix

--- main.tf
+++ main.tf
@@ -21,15 +21,14 @@ resource "azurerm_resource_group" "storage" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                        = "sa${replace(local.prefix, "-", "")}storage"
-  resource_group_name         = azurerm_resource_group.storage.name
-  location                    = azurerm_resource_group.storage.location
-  account_tier                = "Standard"
-  account_replication_type    = "LRS"
-  account_kind                = "StorageV2"
-  access_tier                 = "Hot"
-  allow_blob_public_access    = true
-  supports_https_traffic_only = true
+  name                     = "sa${replace(local.prefix, "-", "") }storage"
+  resource_group_name      = azurerm_resource_group.storage.name
+  location                 = azurerm_resource_group.storage.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
+  enable_https_traffic_only = true
 }
 
 resource "azurerm_storage_container" "storage_containers" {
